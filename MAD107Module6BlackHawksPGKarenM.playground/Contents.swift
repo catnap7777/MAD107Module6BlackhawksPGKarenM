5238ce9f8c38cd20a8b0569d89a50663e51eb765
km <35552602+catnap7777@users.noreply.github.com>
2020-02-26 13:22:44 -0600

    changed to dictionary with tuple as value

--- MAD107Module6BlackHawksPGKarenM.playground/Contents.swift
+++ MAD107Module6BlackHawksPGKarenM.playground/Contents.swift
@@ -6,174 +6,175 @@ var str = "Hello, playground"
 //var players = ["DRAKE CAGGIULA": [
 //                "number":"91",
 //                "age":"25",
-//                "born":"Jun 20, 1994",
+//                "bday":"Jun 20, 1994",
 //                "height":"5'10",
 //                "country":"CAN"
 //                ],
 //               "RYAN CARPENTER": [
 //                "number":"22",
 //                "age":"29",
-//                "born":"Jan 18, 1991",
+//                "bday":"Jan 18, 1991",
 //                "height":"6'0",
 //                "country":"USA"
 //                ],
 //               "KIRBY DACH": [
 //                "number":"77",
 //                "age":"19",
-//                "born":"Jan 21, 2001",
+//                "bday":"Jan 21, 2001",
 //                "height":"6'4",
 //                "country":"CAN"
 //                ],
 //               "ALEX DEBRINCAT": [
 //                "number":"12",
 //                "age":"22",
-//                "born":"Dec 18, 1997",
+//                "bday":"Dec 18, 1997",
 //                "height":"5'7",
 //                "country":"USA"
 //                ],
 //               "MATTHEW HIGHMORE": [
 //                "number":"36",
 //                "age":"23",
-//                "born":"Feb 27, 1996",
+//                "bday":"Feb 27, 1996",
 //                "height":"5'11",
 //                "country":"CAN"
 //                ],
 //               "DAVID KAMPF": [
 //                "number":"64",
 //                "age":"25",
-//                "born":"Jan 12, 1995",
+//                "bday":"Jan 12, 1995",
 //                "height":"6'2",
 //                "country":"CZE"
 //                ],
 //               "PATRICK KANE": [
 //                "number":"88",
 //                "age":"31",
-//                "born":"Nov 19, 1988",
+//                "bday":"Nov 19, 1988",
+//                "height":"5'10",
 //                "country":"USA"
 //                ],
 //               "DOMINIK KUBALIK": [
 //                "number":"8",
 //                "age":"24",
-//                "born":"Aug 21, 1995",
+//                "bday":"Aug 21, 1995",
 //                "height":"6'2",
 //                "country":"CZE"
 //                ],
 //               "ALEX NYLANDER": [
 //                "number":"92",
 //                "age":"21",
-//                "born":"Mar 2, 1998",
+//                "bday":"Mar 2, 1998",
 //                "height":"6'1",
 //                "country":"CAN"
 //                ],
 //               "BRANDON SAAD": [
 //                "number":"20",
 //                "age":"27",
-//                "born":"Oct 27, 1992",
+//                "bday":"Oct 27, 1992",
 //                "height":"6'1",
 //                "country":"USA"
 //                ],
 //               "ANDREW SHAW": [
 //                "number":"65",
 //                "age":"28",
-//                "born":"Jul 20, 1991",
+//                "bday":"Jul 20, 1991",
 //                "height":"5'11",
 //                "country":"CAN"
 //                ],
 //               "ZACK SMITH": [
 //                "number":"15",
 //                "age":"31",
-//                "born":"Apr 5, 1988",
+//                "bday":"Apr 5, 1988",
 //                "height":"6'2",
 //                "country":"CAN"
 //                ],
 //               "DYLAN STROME": [
 //                "number":"17",
 //                "age":"22",
-//                "born":"Mar 7, 1997",
+//                "bday":"Mar 7, 1997",
 //                "height":"6'3",
 //                "country":"CAN"
 //                ],
 //               "JONATHAN TOEWS": [
 //                "number":"19",
 //                "age":"31",
-//                "born":"Apr 29, 1988",
+//                "bday":"Apr 29, 1988",
 //                "height":"6'2",
 //                "country":"CAN"
 //                ],
 //               "ADAM BOQVIST": [
 //                "number":"27",
 //                "age":"19",
-//                "born":"Aug 15, 2000",
+//                "bday":"Aug 15, 2000",
 //                "height":"5'11",
 //                "country":"SWE"
 //                ],
 //               "LUCAS CARLSSON": [
 //                "number":"46",
 //                "age":"22",
-//                "born":"Jul 5, 1997",
+//                "bday":"Jul 5, 1997",
 //                "height":"6'0",
 //                "country":"SWE"
 //                ],
 //               "CALVIN DE HAAN": [
 //                "number":"44",
 //                "age":"28",
-//                "born":"May 9, 1991",
+//                "bday":"May 9, 1991",
 //                "height":"6'1",
 //                "country":"CAN"
 //                ],
 //               "DUNCAN KEITH": [
 //                "number":"2",
 //                "age":"36",
-//                "born":"Jul 16, 1983",
+//                "bday":"Jul 16, 1983",
 //                "height":"6'1",
 //                "country":"CAN"
 //                ],
 //               "SLATER KOEKKOEK": [
 //                "number":"68",
 //                "age":"26",
-//                "born":"Feb 18, 1994",
+//                "bday":"Feb 18, 1994",
 //                "height":"6'2",
 //                "country":"CAN"
 //                ],
 //               "OLLI MAATTA": [
 //                "number":"6",
 //                "age":"25",
-//                "born":"Aug 22, 1994",
+//                "bday":"Aug 22, 1994",
 //                "height":"6'2",
 //                "country":"FIN"
 //                ],
 //               "CONNOR MURPHY": [
 //                "number":"5",
 //                "age":"26",
-//                "born":"Mar 26, 1993",
+//                "bday":"Mar 26, 1993",
 //                "height":"6'4",
 //                "country":"USA"
 //                ],
 //               "BRENT SEABROOK": [
 //                "number":"7",
 //                "age":"34",
-//                "born":"Apr 20, 1985",
+//                "bday":"Apr 20, 1985",
 //                "height":"6'3",
 //                "country":"CAN"
 //                ],
 //               "NICK SEELER": [
 //                "number":"55",
 //                "age":"26",
-//                "born":"Jun 3, 1993",
+//                "bday":"Jun 3, 1993",
 //                "height":"6'2",
 //                "country":"USA"
 //                ],
 //               "COREY CRAWFORD": [
 //                "number":"50",
 //                "age":"35",
-//                "born":"Dec 31, 1984",
+//                "bday":"Dec 31, 1984",
 //                "height":"6'2",
 //                "country":"CAN"
 //                ],
 //               "MALCOLM SUBBAN": [
 //                "number":"30",
 //                "age":"26",
-//                "born":"Dec 21, 1993",
+//                "bday":"Dec 21, 1993",
 //                "height":"6'2",
 //                "country":"CAN"
 //                ]
@@ -182,9 +183,9 @@ var str = "Hello, playground"
 //var playersArrayConverted = players
 //print(playersArrayConverted)
 
-//var players2 = ["DRAKE CAGGIULA": (number:"91",age:"25",born:"Jun 20, 1994",height:"5'10",country:"CAN"),
-//                "RYAN CARPENTER": (number:"22",age:"29",born:"Jan 18, 1991",hieght:"6'0",country:"USA"),
-//                "KIRBY DACH": (number:"77",age:"19",born:"Jan 21, 2001",height:"6'4",country:"CAN")
+//var players2 = ["DRAKE CAGGIULA": (number:"91",age:"25",bday:"Jun 20, 1994",height:"5'10",country:"CAN"),
+//                "RYAN CARPENTER": (number:"22",age:"29",bday:"Jan 18, 1991",hieght:"6'0",country:"USA"),
+//                "KIRBY DACH": (number:"77",age:"19",bday:"Jan 21, 2001",height:"6'4",country:"CAN")
 //
 //    ] as [String : AnyObject]
 //
@@ -215,6 +216,35 @@ var players8Dictionary = ["91": (name:"DRAKE",age:"54",bday:"dec 15 1965",ht:"5.
                           "93": (name:"ALMA",age:"55",bday:"dec 16 1965",ht:"5.8",country:"USA")
 ]
 
+var players8Dictionary2 = ["91": (name:"DRAKE CAGGIULA",age:"25",bday:"Jun 20, 1994",height:"5'10",country:"CAN"),
+                          "22": (name:"RYAN CARPENTER",age:"29",bday:"Jan 18, 1991",height:"6'0",country:"USA"),
+                          "77": (name:"KIRBY DACH",age:"19",bday:"Jan 21, 2001",height:"6'4",country:"CAN"),
+                          "12": (name:"ALEX DEBRINCAT",age:"22",bday:"Dec 18, 1997",height:"5'7",country:"USA"),
+                          "36": (name:"MATTHEW HIGHMORE",age:"23",bday:"Feb 27, 1996",height:"5'11",country:"CAN"),
+                          "64": (name:"DAVID KAMPF",age:"25",bday:"Jan 12, 1995",height:"6'2",country:"CZE"),
+                          "88": (name:"PATRICK KANE",age:"31",bday:"Nov 19, 1988",height:"5'10",country:"USA"),
+                          "8": (name:"DOMINIK KUBALIK",age:"24",bday:"Aug 21, 1995",height:"6'2",country:"CZE"),
+                          "92": (name:"ALEX NYLANDER",age:"21",bday:"Mar 2, 1998",height:"6'1",country:"CAN"),
+                          "20": (name:"BRANDON SAAD",age:"27",bday:"Oct 27, 1992",height:"6'1",country:"USA"),
+                          "65": (name:"ANDREW SHAW",age:"28",bday:"Jul 20, 1991",height:"5'11",country:"CAN"),
+                          "15": (name:"ZACK SMITH",age:"31",bday:"Apr 5, 1988",height:"6'2",country:"CAN"),
+                          "17": (name:"DYLAN STROME",age:"22",bday:"Mar 7, 1997",height:"6'3",country:"CAN"),
+                          "19": (name:"JONATHAN TOEWS",age:"31",bday:"Apr 29, 1988",height:"6'2",country:"CAN"),
+                          "27": (name:"ADAM BOQVIST",age:"19",bday:"Aug 15, 2000",height:"5'11",country:"SWE"),
+                          "46": (name:"LUCAS CARLSSON",age:"22",bday:"Jul 5, 1997",height:"6'0",country:"SWE"),
+                          "44": (name:"CALVIN DE HAAN",age:"28",bday:"May 9, 1991",height:"6'1",country:"CAN"),
+                          "2": (name:"DUNCAN KEITH",age:"36",bday:"Jul 16, 1983",height:"6'1",country:"CAN"),
+                          "68": (name:"SLATER KOEKKOEK",age:"26",bday:"Feb 18, 1994",height:"6'2",country:"CAN"),
+                          "6": (name:"OLLI MAATTA",age:"25",bday:"Aug 22, 1994",height:"6'2",country:"FIN"),
+                          "5": (name:"CONNOR MURPHY",age:"26",bday:"Mar 26, 1993",height:"6'4",country:"USA"),
+                          "7": (name:"BRENT SEABROOK",age:"34",bday:"Apr 20, 1985",height:"6'3",country:"CAN"),
+                          "55": (name:"NICK SEELER",age:"26",bday:"Jun 3, 1993",height:"6'2",country:"USA"),
+                          "50": (name:"COREY CRAWFORD",age:"35",bday:"Dec 31, 1984",height:"6'2",country:"CAN"),
+                          "30": (name:"MALCOLM SUBBAN",age:"26",bday:"Dec 21, 1993",height:"6'2",country:"CAN")
+   ]
+
+
+
 var testVar8 = players8Dictionary["91"]?.2
 
 print("****** this is testVar8: \(String(describing: testVar8))")
@@ -235,6 +265,8 @@ for item in testTuplePlayersInc {
     
 }
 
+
+
 //Array of Tuples
 //let tupleArray = [("A", 123), ("B", 789), ("C", 567), ("D", 432)]
 //let tupleArrayInc = tupleArray.sorted(by: { $0.1 < $1.1 })
@@ -245,10 +277,10 @@ for item in testTuplePlayersInc {
 
 //var Name: String = "kam"
 //
-//var players3 = [Name:(number:String, age:String, born:String, height:String, country:String).self] =
+//var players3 = [Name:(number:String, age:String, bday:String, height:String, country:String).self] =
 //    [   .Ryan:("001","30","Dec 15, 1965", "5'7", "USA")]
 //
-//var myObject = (number:String,age:String, born:String, height:String, country:String).self
+//var myObject = (number:String,age:String, bday:String, height:String, country:String).self
 //var players4 = [Name:myObject]
 //var dic3 = [String:MyObject]()
 
