9d94b2a8993dbdc2fdcf72d5ea1c8b596146fa23
km <35552602+catnap7777@users.noreply.github.com>
2020-02-25 19:53:10 -0600

    initial commit

--- MAD107Module6BlackHawksPGKarenM.playground/Contents.swift
+++ MAD107Module6BlackHawksPGKarenM.playground/Contents.swift
@@ -6,156 +6,174 @@ var str = "Hello, playground"
 var players = ["DRAKE CAGGIULA": [
                 "number":"91",
                 "age":"25",
+                "born":"Jun 20, 1994",
                 "height":"5'10",
                 "country":"CAN"
                 ],
                "RYAN CARPENTER": [
                 "number":"22",
                 "age":"29",
+                "born":"Jan 18, 1991",
                 "height":"6'0",
                 "country":"USA"
                 ],
                "KIRBY DACH": [
                 "number":"77",
                 "age":"19",
+                "born":"Jan 21, 2001",
                 "height":"6'4",
                 "country":"CAN"
                 ],
                "ALEX DEBRINCAT": [
                 "number":"12",
                 "age":"22",
+                "born":"Dec 18, 1997",
                 "height":"5'7",
                 "country":"USA"
                 ],
                "MATTHEW HIGHMORE": [
                 "number":"36",
                 "age":"23",
+                "born":"Feb 27, 1996",
                 "height":"5'11",
                 "country":"CAN"
                 ],
                "DAVID KAMPF": [
                 "number":"64",
                 "age":"25",
+                "born":"Jan 12, 1995",
                 "height":"6'2",
                 "country":"CZE"
                 ],
                "PATRICK KANE": [
                 "number":"88",
                 "age":"31",
-                "height":"5'10",
+                "born":"Nov 19, 1988",
                 "country":"USA"
                 ],
                "DOMINIK KUBALIK": [
                 "number":"8",
                 "age":"24",
+                "born":"Aug 21, 1995",
                 "height":"6'2",
                 "country":"CZE"
                 ],
                "ALEX NYLANDER": [
                 "number":"92",
                 "age":"21",
+                "born":"Mar 2, 1998",
                 "height":"6'1",
                 "country":"CAN"
                 ],
                "BRANDON SAAD": [
                 "number":"20",
                 "age":"27",
+                "born":"Oct 27, 1992",
                 "height":"6'1",
                 "country":"USA"
                 ],
                "ANDREW SHAW": [
                 "number":"65",
                 "age":"28",
+                "born":"Jul 20, 1991",
                 "height":"5'11",
                 "country":"CAN"
                 ],
                "ZACK SMITH": [
                 "number":"15",
                 "age":"31",
+                "born":"Apr 5, 1988",
                 "height":"6'2",
                 "country":"CAN"
                 ],
                "DYLAN STROME": [
                 "number":"17",
                 "age":"22",
+                "born":"Mar 7, 1997",
                 "height":"6'3",
                 "country":"CAN"
                 ],
                "JONATHAN TOEWS": [
                 "number":"19",
                 "age":"31",
+                "born":"Apr 29, 1988",
                 "height":"6'2",
                 "country":"CAN"
                 ],
                "ADAM BOQVIST": [
                 "number":"27",
                 "age":"19",
+                "born":"Aug 15, 2000",
                 "height":"5'11",
                 "country":"SWE"
                 ],
                "LUCAS CARLSSON": [
                 "number":"46",
                 "age":"22",
+                "born":"Jul 5, 1997",
                 "height":"6'0",
                 "country":"SWE"
                 ],
                "CALVIN DE HAAN": [
                 "number":"44",
-                "age":"26",
-                "height":"6'1",
-                "country":"SWE"
-                ],
-               "CALVIN DE HAAN": [
-                "number":"44",
                 "age":"28",
+                "born":"May 9, 1991",
                 "height":"6'1",
                 "country":"CAN"
                 ],
                "DUNCAN KEITH": [
                 "number":"2",
                 "age":"36",
+                "born":"Jul 16, 1983",
                 "height":"6'1",
                 "country":"CAN"
                 ],
                "SLATER KOEKKOEK": [
                 "number":"68",
                 "age":"26",
+                "born":"Feb 18, 1994",
                 "height":"6'2",
                 "country":"CAN"
                 ],
                "OLLI MAATTA": [
                 "number":"6",
                 "age":"25",
+                "born":"Aug 22, 1994",
                 "height":"6'2",
                 "country":"FIN"
                 ],
                "CONNOR MURPHY": [
                 "number":"5",
                 "age":"26",
+                "born":"Mar 26, 1993",
                 "height":"6'4",
                 "country":"USA"
                 ],
                "BRENT SEABROOK": [
                 "number":"7",
                 "age":"34",
+                "born":"Apr 20, 1985",
                 "height":"6'3",
                 "country":"CAN"
                 ],
                "NICK SEELER": [
                 "number":"55",
                 "age":"26",
+                "born":"Jun 3, 1993",
                 "height":"6'2",
                 "country":"USA"
                 ],
                "COREY CRAWFORD": [
                 "number":"50",
                 "age":"35",
+                "born":"Dec 31, 1984",
                 "height":"6'2",
                 "country":"CAN"
                 ],
                "MALCOLM SUBBAN": [
                 "number":"30",
                 "age":"26",
+                "born":"Dec 21, 1993",
                 "height":"6'2",
                 "country":"CAN"
                 ]
